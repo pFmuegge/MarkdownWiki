87877bda941135cf52d4ce857ac47c7ded3c0217
pFmuegge <65241954+pFmuegge@users.noreply.github.com>
2025-05-27 21:04:33 +0200

    init project

---
+++ Components/MarkdownToken.cs
@@ -0,0 +1,29 @@
+﻿namespace MarkdownWiki.Components;
+
+public class MarkdownToken(string token, bool isLink)
+{
+    public string Value { get; set; } = token;
+
+    public bool IsLink { get; set; } = isLink;
+
+    public string FullValue => IsLink ? "![[" + Value + "]]" : "[[" + Value + "]]";
+
+    public string Cleaned => CleanValue();
+
+    public string Refine(List<MenuItem> menuitems)
+    {
+        return Value;
+    }
+
+    private string CleanValue()
+    {
+        if (Value.Contains("|"))
+        {
+            return Value.Split("|")[0];
+        }
+        else
+        {
+            return Value;
+        }
+    }
+}
---
+++ Components/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿namespace MarkdownWiki.Components
+{
+    public class MenuItem
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
+        public string Extension { get; set; } = string.Empty;
+        public bool IsFolder { get; set; }
+        public bool IsPicture { get; set; }
+        public List<MenuItem> Items { get; set; } = [];
+    }
+}
